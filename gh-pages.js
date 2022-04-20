019f600fd433236ba838fd1225d35bf1f5faf34a
ldmichae <l96mich@gmail.com>
2022-04-20 00:13:01 -0400

    .

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'main',
+        branch: 'gh-pages',
         repo: 'https://github.com/ldmichae/minesvelter.git', // Update to point to your repository  
         user: {
             name: 'Logan Michaels', // update to use your name
