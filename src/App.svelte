22ff64f0049082a18bb26b6fb6f73584cce8b483
ldmichae <l96mich@gmail.com>
2022-04-20 22:49:47 -0400

    bomb on 5

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script>
+	import Square from './Square.svelte'
 	export let edgeLen
 	export let inputDisabled = false
 	export const activeText = "Start Game!"
 	export const inactiveText = "Change Board"
 	$: columnSize = `${screen.width * 0.33 / edgeLen}px`
+	
+	
 </script>
 
 <main>
 	<h1>Hello!</h1>
-	<input type = "number" min = "3" max = "10" bind:value={edgeLen} disabled={inputDisabled}>
+	<input type = "number" min = "3" max = "10" bind:value={edgeLen} disabled={inputDisabled} />
 	<button on:click={() => {inputDisabled = !inputDisabled}}>
 		{#if inputDisabled}
 			Change Board
@@ -18,9 +21,16 @@
 		{/if}
 	</button>
 	<div class = "minefield" style="--colWidth: {columnSize}; --edges: {edgeLen}"> 
-		{#each {length: edgeLen ** 2} as _, i}
-			<div class="patch">{i+1}</div>
-		{/each}
+		{#key edgeLen}
+			{#each {length: edgeLen ** 2} as _, i}
+				{#if i === 5}
+					<Square class = "square" hasBomb={true} />
+				{/if}
+				{#if i !== 5}
+					<Square class = "square" hasBomb={false} />
+				{/if}
+			{/each}
+		{/key}
 	</div>
 </main>
 
@@ -29,10 +39,5 @@
 		display: grid;
 		grid-template-columns: repeat(var(--edges), var(--colWidth));
 		grid-template-rows: repeat(var(--edges), var(--colWidth));
-	}
-	.patch{
-		border: 1px;
-		border-color: black;
-		border-style: solid;
 	}
 </style>
---
+++ src/Square.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { tick} from 'svelte';
+    export let hasBomb
+    export let searched = false
+
+</script>
+
+
+<main>
+    <div class = "square" on:click={() => { searched = true}}>
+        {#if searched === true && hasBomb === true }
+            X
+        {/if}
+        {#if searched === true && hasBomb === false}
+            O
+        {/if}
+    </div>
+</main>
+
+
+<style>
+    .square{
+        height: 100%;
+        width: 100% ;
+		border: 1px;
+		border-color: black;
+		border-style: solid;
+        cursor: pointer;
+        display: grid;
+        align-items: center;
+        justify-self: center;
+        text-align: center;
+	}
+</style>
